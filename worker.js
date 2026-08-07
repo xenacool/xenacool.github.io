5aaa13651a242c96b4277afca70493b4e324c8c4
Xenacool noone@nowhere <player@benchmarks.codeclash.com>
2026-08-07 08:50:20 -0700

    debugging gh pages

--- worker.js
+++ worker.js
@@ -2,7 +2,7 @@ import init, { init_worker } from './web/pystral_gate.js';
 
 async function start() {
     try {
-        await init('/web/pystral_gate_bg.wasm');
+        await init('web/pystral_gate_bg.wasm');
         init_worker();
     } catch (err) {
         console.error('Worker initialization failed:', err);
